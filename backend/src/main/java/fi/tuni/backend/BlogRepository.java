26dce6d9a845f913b6596854feda351501aa20c7
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-14 11:08:10 +0200

    BlogRepository: Add method getArticlesByAuthorID()

--- backend/src/main/java/fi/tuni/backend/BlogRepository.java
+++ backend/src/main/java/fi/tuni/backend/BlogRepository.java
@@ -2,5 +2,8 @@ package fi.tuni.backend;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface BlogRepository extends CrudRepository<Article, Integer> {
+    public List<Article> getArticlesByAuthorID(int authorID);
 }
