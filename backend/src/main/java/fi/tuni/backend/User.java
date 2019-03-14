cdda10c6d969e7ac23604e37352d4a0ae8aeafe7
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-14 10:23:54 +0200

    User: Add variables

--- backend/src/main/java/fi/tuni/backend/User.java
+++ backend/src/main/java/fi/tuni/backend/User.java
@@ -1,8 +1,40 @@
 package fi.tuni.backend;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private int id;
 
+    private String firstName;
+    private String lastName;
+
+    public int getId() {
+        return id;
+    }
+
+    public User(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
