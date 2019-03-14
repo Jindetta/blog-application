e9481a601b9e3e36cfd84cef0a3cff401e80add1
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-14 21:58:11 +0200

    App: Add primeflex stylesheet import
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,14 +4,18 @@ import './App.css';
 import 'primereact/resources/themes/nova-light/theme.css';
 import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
+import 'primeflex/primeflex.css';
 
 import Ribbon from "./components/Ribbon";
+import ArticleView from "./components/ArticleView";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Ribbon/>
+      <div className="p-grid p-justify-center">
+        <div className="p-col-8">
+          <ArticleView/>
+        </div>
       </div>
     );
   }
