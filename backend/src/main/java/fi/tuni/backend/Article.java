b54cb43bb4bf1088d2ec99ef63794a310b1e1f56
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-14 10:22:19 +0200

    Article: Add variables

--- backend/src/main/java/fi/tuni/backend/Article.java
+++ backend/src/main/java/fi/tuni/backend/Article.java
@@ -1,8 +1,49 @@
 package fi.tuni.backend;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 
 @Entity
 public class Article {
+    @Id
+    @GeneratedValue
+    private int id;
+    private String title;
+    private String content;
+    private User user;
 
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Article(String title, String content, User user) {
+        this.title = title;
+        this.content = content;
+        this.user = user;
+    }
 }
