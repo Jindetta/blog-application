6d20afae641217aff77f302eebd5e85f33e712fd
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-20 18:40:25 +0200

    CORSConfiguration: Add
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

---
+++ backend/src/main/java/fi/tuni/backend/CORSConfiguration.java
@@ -0,0 +1,15 @@
+package fi.tuni.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+public class CORSConfiguration implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "OPTIONS", "POST", "PUT", "DELETE");
+    }
+}
