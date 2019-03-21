83191be104caa1a4fa654a61a4e58399c9e75bac
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 10:18:23 +0200

    SearchReducer: Add file
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

---
+++ frontend/src/reducers/SearchReducer.js
@@ -0,0 +1,15 @@
+const DEFAULT_STATE = {
+  SEARCH_VALUE: 'default value'
+};
+
+export default (state = DEFAULT_STATE, action) => {
+  switch (action.type) {
+    case 'SEARCH_VALUE': {
+      console.log(action);
+
+      state = {...state, SEARCH_VALUE: action.payload};
+      break;
+    }
+  }
+  return state;
+}
