63d478ed85a8efbb93ffde7c1aa3852088c9ec84
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 10:27:39 +0200

    SearchView: Add ability to input text to <InputText>
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/components/SearchView.js
+++ frontend/src/components/SearchView.js
@@ -10,6 +10,12 @@ import * as actions from '../actions/SearchActions';
 class SearchView extends Component{
   constructor(props) {
     super(props);
+    this.textChanged = this.textChanged.bind(this);
+  }
+
+  textChanged(event) {
+    console.log(event.target.value);
+    this.props.dispatch(actions.setSearchValue(event.target.value))
   }
 
   render() {
@@ -18,8 +24,8 @@ class SearchView extends Component{
         <div className="p-col-12" id="page">
           <div className="p-col-4">
             <h3>Search</h3>
-            <Button label="Search" icon="pi pi-fw pi-search"/>
-            <InputText placeholder="Keyword"/>
+            <Button label="Search" icon="pi pi-fw pi-search" onClick={() => console.log(this.props.SEARCH_VALUE)}/>
+            <InputText placeholder="Keyword" value={this.props.SEARCH_VALUE} onChange={this.textChanged}/>
           </div>
           <div className="p-col-12" id="results">
             <p>Search results here</p>
