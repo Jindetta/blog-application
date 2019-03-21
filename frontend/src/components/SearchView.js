5de0b9a2c370bba73c514a2ab378124a6011d8dc
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 10:07:54 +0200

    SearchView: Add search bar and results to ui
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/components/SearchView.js
+++ frontend/src/components/SearchView.js
@@ -3,6 +3,8 @@ import React,{Component} from "react";
 import {connect} from "react-redux";
 
 import './SearchView.css';
+import {Button} from "primereact/button";
+import {InputText} from "primereact/inputtext";
 
 class SearchView extends Component{
   constructor(props) {
@@ -13,7 +15,14 @@ class SearchView extends Component{
     return (
       <div className="p-grid">
         <div className="p-col-12" id="page">
-          <p>Hello world!</p>
+          <div className="p-col-4">
+            <h3>Search</h3>
+            <Button label="Search" icon="pi pi-fw pi-search"/>
+            <InputText placeholder="Keyword"/>
+          </div>
+          <div className="p-col-12" id="results">
+            <p>Search results here</p>
+          </div>
         </div>
       </div>
     );
