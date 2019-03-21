98496a798f27d76a33c7aa57b6f1ff95fee4718b
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 11:41:52 +0200

    BlogRepository: Add find method for title and content
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- backend/src/main/java/fi/tuni/backend/BlogRepository.java
+++ backend/src/main/java/fi/tuni/backend/BlogRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.repository.CrudRepository;
 
 public interface BlogRepository extends CrudRepository<Article, Integer> {
     Iterable<Article> findArticlesByAuthorIdEquals(int authorId);
+    Iterable<Article> findArticlesByTitleContainingIgnoreCaseOrContentContainingIgnoreCase(String title, String content);
+
 }
