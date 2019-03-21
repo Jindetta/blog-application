0703e7c0c7cd0cd2062a760c42caa35532ecb050
Tuukkaj <juusela.tuukka@gmail.com>
2019-03-21 09:36:26 +0200

    App: Change col size to 10
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,14 +6,13 @@ import 'primereact/resources/primereact.min.css';
 import 'primeicons/primeicons.css';
 import 'primeflex/primeflex.css';
 
-import Ribbon from "./components/Ribbon";
 import ArticleView from "./components/ArticleView";
 
 class App extends Component {
   render() {
     return (
       <div className="p-grid p-justify-center">
-        <div className="p-col-8">
+        <div className="p-col-10">
           <ArticleView/>
         </div>
       </div>
