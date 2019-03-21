1b5ab86f1babe0a0ef0ec2a5873618ecfcd87cd2
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-21 23:45:17 +0200

    App: Change view based on state
    - Bind component to Ribbon state
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import {connect} from "react-redux";
 
 import './App.css';
 import 'primereact/resources/themes/nova-light/theme.css';
@@ -8,17 +9,41 @@ import 'primeflex/primeflex.css';
 
 import ArticleView from "./components/ArticleView";
 import SearchView from "./components/SearchView";
+import Ribbon from './components/Ribbon';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getView = this.getView.bind(this);
+  }
+
+  getView() {
+    switch (this.props.ACTIVE_MENU) {
+      case 2: {
+        return <SearchView/>;
+      }
+    }
+
+    return <ArticleView/>;
+  }
+
   render() {
     return (
-      <div className="p-grid p-justify-center">
-        <div className="p-col-10">
-          <ArticleView/>
+      <div>
+        <div className="p-grid p-justify-center">
+          <div className="p-col-10">
+            <Ribbon/>
+          </div>
+        </div>
+        <div className="p-grid p-justify-center">
+          <div className="p-col-10">
+            {this.getView()}
+          </div>
         </div>
       </div>
     );
   }
 }
 
-export default App;
+export default connect(data => data.ribbon)(App);
