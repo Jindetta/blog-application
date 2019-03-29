f13168734d5c9702d245f2cc8b30caac6c800708
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-03-29 20:02:31 +0200

    CommentRepository: Add methods for finding comments by article or author
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- backend/src/main/java/fi/tuni/backend/CommentRepository.java
+++ backend/src/main/java/fi/tuni/backend/CommentRepository.java
@@ -2,5 +2,12 @@ package fi.tuni.backend;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface CommentRepository extends CrudRepository<Comment, Integer> {
+    Iterable<Comment> findByArticleId(int articleId);
+    Optional<Comment> findByIdAndArticleId(int commentId, int articleId);
+
+    Iterable<Comment> findByAuthorId(int authorId);
+    Optional<Comment> findByIdAndAuthorId(int commentId, int authorId);
 }
