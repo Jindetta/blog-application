152bb2a456bef2b3dfd10f6b473701cc614c8b4c
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-02 22:20:12 +0300

    ArticleView: Fetch user and comment data to article
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/components/ArticleView.js
+++ frontend/src/components/ArticleView.js
@@ -16,6 +16,7 @@ class ArticleView extends Component {
     this.fetchUserData = this.fetchUserData.bind(this);
     this.renderArticle = this.renderArticle.bind(this);
     this.fetchAllData = this.fetchAllData.bind(this);
+    this.fetchUserData = this.fetchUserData.bind(this);
 
     if (process.env.NODE_ENV === "development") {
       this.fetchUrl = "http://localhost:8080";
@@ -32,17 +33,22 @@ class ArticleView extends Component {
     fetch(`${this.fetchUrl}/blogs/${this.props.match.params.id}`)
       .then(response => response.json())
       .then(data => this.props.dispatch(actions.setBlogData(data)))
-      .then(() => this.fetchUserData());
+      .then(() => {this.fetchUserData(); this.fetchCommentData()});
   }
 
-  fetchUserData(userData) {
-    if(userData) {
-      return this.props.AUTHOR_DATA.filter(item => item.id === userData.id);
-    } else {
-      fetch(`${this.fetchUrl}/users`)
-        .then(response => response.json())
-        .then(data => this.props.dispatch(actions.setAuthorData(data)));
-    }
+  fetchCommentData() {
+    fetch(`${this.fetchUrl}/blogs/${this.props.match.params.id}/comments`)
+      .then(response => response.json())
+      .then(data => this.props.dispatch(actions.setCommentData(data)))
+  }
+
+  fetchUserData() {
+    fetch(`${this.fetchUrl}/users/${this.props.BLOG_DATA.author_id}`)
+      .then(response => response.json())
+      .then(data => {
+        console.log(data)
+        this.props.dispatch(actions.setAuthorData(data))
+      });
   }
 
   renderArticle(data) {
@@ -55,7 +61,11 @@ class ArticleView extends Component {
     return (
       <div key={data.id} className="article">
         <h1>{data.title}</h1>
+        <p>{this.props.AUTHOR_DATA?this.props.AUTHOR_DATA.firstName + " " + this.props.AUTHOR_DATA.lastName:"author"}</p>
         <p>{data.content}</p>
+        <div>
+          {this.props.COMMENT_DATA?this.props.COMMENT_DATA.map(comment => <p className="comment">{comment.comment}</p>): "No comments"}
+        </div>
       </div>
     );
   }
@@ -79,7 +89,14 @@ class ArticleView extends Component {
   }
 
   render() {
-    return <div id="page">{this.getContent()}</div>;
+    return <div id="page">
+      <div>
+        {this.getContent()}
+      </div>
+      <div>
+        {}
+      </div>
+    </div>;
   }
 
   static getErrorMessage(data) {
