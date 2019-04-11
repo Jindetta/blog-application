151d840c4b83eabf99be6115905c327c5495b583
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-11 11:58:41 +0300

    ArticleView: Add ability to remove comments as admin
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/components/ArticleView.js
+++ frontend/src/components/ArticleView.js
@@ -21,6 +21,9 @@ class ArticleView extends Component {
     this.fetchUserData = this.fetchUserData.bind(this);
     this.postComment = this.postComment.bind(this);
     this.renderCommenting = this.renderCommenting.bind(this);
+    this.renderComment = this.renderComment.bind(this);
+    this.renderCommentAdminButton = this.renderCommentAdminButton.bind(this);
+    this.deleteComment = this.deleteComment.bind(this);
 
     this.state = {comment: ""}
     if (process.env.NODE_ENV === "development") {
@@ -83,10 +86,40 @@ class ArticleView extends Component {
         <p>{this.props.AUTHOR_DATA?this.props.AUTHOR_DATA.username:"author"}</p>
         <p>{data.content}</p>
         <div>
-          {this.props.COMMENT_DATA?this.props.COMMENT_DATA.map(comment => <p className="comment">{comment.comment}</p>): "No comments"}
+          {this.props.COMMENT_DATA?this.props.COMMENT_DATA.map(comment => this.renderComment(comment)): "No comments"}
         </div>
       </div>
     );
+  }
+
+  renderComment(comment) {
+      return <div className="comment">
+        <div className="p-grid p-align-center">
+          <div className="p-col-10">
+            <p>{comment.comment}</p>
+          </div>
+          <div className="p-col-2" align="center">
+          {this.renderCommentAdminButton(comment.link)}
+          </div>
+        </div>
+      </div>
+  }
+
+  renderCommentAdminButton(commentUrl) {
+    if(this.props.permits === "ADMIN") {
+      return <Button icon="pi pi-times" className="p-button-danger" onClick={() => this.deleteComment(commentUrl)}/>
+    } else {
+      return ""
+    }
+  }
+
+  deleteComment(commentUrl) {
+    const url = this.fetchUrl + commentUrl;
+    fetch(url, {
+      method: "DELETE",
+      credentials: "include",
+    }).then(response => console.log(response))
+      .then(() => window.location.reload())
   }
 
   getContent() {
