5e4b039daaadee20e6d0f79937f28bf0f8582fc0
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-24 01:09:25 +0300

    LoginEntryPoint: Update commence() implementation
    - Add JavaDoc dummies to methods
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- backend/src/main/java/fi/tuni/backend/LoginEntryPoint.java
+++ backend/src/main/java/fi/tuni/backend/LoginEntryPoint.java
@@ -7,22 +7,29 @@ import org.springframework.stereotype.Component;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 /**
  *
  */
 @Component
 public class LoginEntryPoint extends BasicAuthenticationEntryPoint {
+
+    /**
+     *
+     * @param request
+     * @param response
+     * @param authEx
+     */
     @Override
-    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx)
-                                                                            throws IOException {
-        response.addHeader("WWW-Authenticate", "Basic realm=\"" + getRealmName() + "\"");
+    public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authEx) throws IOException {
+        response.addHeader("WWW-Authenticate", "Basic realm=" + getRealmName());
         response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-        PrintWriter writer = response.getWriter();
-        writer.println("HTTP Status 401 - " + authEx.getMessage());
     }
 
+    /**
+     *
+     * @throws Exception
+     */
     @Override
     public void afterPropertiesSet() throws Exception {
         setRealmName("Kesahessut Blog application");
