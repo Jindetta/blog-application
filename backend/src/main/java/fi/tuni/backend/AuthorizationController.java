4158cea4e3eec69deba13ef0a6e19dcc07c0cc9a
Joonas Lauhala <joonas.lauhala@cs.tamk.fi>
2019-04-24 01:12:26 +0300

    AuthenticationController: Implement promptAuthentication()
    - Add JavaDoc dummies to methods
    
    Signed-off-by: Joonas Lauhala <joonas.lauhala@cs.tamk.fi>

--- backend/src/main/java/fi/tuni/backend/AuthorizationController.java
+++ backend/src/main/java/fi/tuni/backend/AuthorizationController.java
@@ -1,9 +1,10 @@
 package fi.tuni.backend;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.Optional;
 
@@ -12,21 +13,39 @@ import java.util.Optional;
  */
 @RestController
 public class AuthorizationController {
+
+    /**
+     *
+     */
     @Autowired
     private UserRepository userRepository;
 
-    @GetMapping("permits")
+    /**
+     *
+     * @param auth
+     * @return
+     */
+    @GetMapping("/permits")
     public Permits getPermissions (Authentication auth) {
         if (auth != null) {
             Optional<User> result = userRepository.findUserByUsername(auth.getName());
 
             return new Permits(
                 result.map(user -> user.isAdmin() ? Permits.PermitTypes.ADMIN : Permits.PermitTypes.USER)
-                      .orElse(Permits.PermitTypes.ANONYMOUS)
-                    , result.isPresent()?result.get().getId():-1
+                      .orElse(Permits.PermitTypes.ANONYMOUS),
+                result.map(User::getId).orElse(-1)
             );
         }
 
         return new Permits(Permits.PermitTypes.ANONYMOUS, -1);
+    }
+
+    /**
+     *
+     * @return
+     */
+    @GetMapping("/authenticate")
+    public ModelAndView promptAuthentication() {
+        return new ModelAndView("redirect:/");
     }
 }
