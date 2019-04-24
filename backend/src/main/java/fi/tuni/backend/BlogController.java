ebd2ba890167752e95daa096f605cffd8527ae94
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-25 00:37:09 +0300

    BlogController: Add JavaDoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- backend/src/main/java/fi/tuni/backend/BlogController.java
+++ backend/src/main/java/fi/tuni/backend/BlogController.java
@@ -14,34 +14,69 @@ import java.util.List;
 import java.util.Optional;
 
 /**
+ * Controller for getting blog data.
  *
+ * @author Joonas Lauhala {@literal <joonas.lauhala@tuni.fi>}
+ *         Tuukka Juusela {@literal <tuukka.juusela@tuni.fi}
+ * @version 20192802
+ * @since   1.8
  */
 @RestController
 @RequestMapping("/api")
 public class BlogController {
 
+    /**
+     * Repository for accessing blog database.
+     */
     @Autowired
     BlogRepository blogRepository;
 
+    /**
+     * Repository for accessing users database.
+     */
     @Autowired
     UserRepository userRepository;
 
+    /**
+     * Repository for accessing comments database.
+     */
     @Autowired
     CommentRepository commentRepository;
 
+    /**
+     * Repository for accessing comments likes database.
+     */
     @Autowired
     LikeRepository likeRepository;
 
+    /**
+     * Return blog with id given in URL.
+     *
+     * @param id Id of blog to return.
+     * @return Blog post with given Id.
+     */
     @GetMapping("blogs/{id:\\d+}")
     public Article getArticle(@PathVariable int id) {
         return blogRepository.findById(id).orElseThrow(() -> new CannotFindTargetException(id, "Cannot find article with id:" + id));
     }
 
+    /**
+     * Gets all blog posts from database.
+     * @return All blog posts in database.
+     */
     @GetMapping("blogs")
     public Iterable<Article> getArticles() {
         return blogRepository.findAll();
     }
 
+    /**
+     * Removes article with given Id.
+     *
+     * @param id Id of blog to delete.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Status NO_CONTENT if successful, throws UserNotAdminException if user was not an admin or
+     * CannotFindTargetException if target was not found.
+     */
     @Secured("ROLE_ADMIN")
     @DeleteMapping("blogs/{id:\\d+}")
     public ResponseEntity<Void> removeArticle(@PathVariable int id, Authentication auth) {
@@ -62,6 +97,14 @@ public class BlogController {
         }
     }
 
+    /**
+     * Adds article to blog database.
+     *
+     * @param title Title of article.
+     * @param content Content of article.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Status CREATED if successful. Throws exception if user is not an admin.
+     */
     @Secured("ROLE_ADMIN")
     @PostMapping("blogs")
     public ResponseEntity<Void> addArticle(String title, String content, Authentication auth) {
@@ -78,6 +121,15 @@ public class BlogController {
         throw new UserNotAdminException("Forbidden action, user with id " + auth.getName() + " is not a admin");
     }
 
+    /**
+     * Edits blog with given id.
+     *
+     * @param id Id of blog to be edited.
+     * @param newTitle Title to replace old title.
+     * @param newContent Content to replace old content.
+     * @param authentication Authentication provided by Spring. Used to determine user.
+     * @return Status CREATED if successful. Throws UserNotAdminException if user is not admin.
+     */
     @Secured("ROLE_ADMIN")
     @PostMapping("blogs/edit/{id:\\d+}")
     public ResponseEntity<Void> editBlog(@PathVariable int id, String newTitle, String newContent, Authentication authentication, UriComponentsBuilder builder) {
@@ -97,11 +149,25 @@ public class BlogController {
         throw new UserNotAdminException("Forbidden action, user " + authentication.getName() + " is not a admin");
     }
 
+    /**
+     * Returns all blog posts containing given parameter in posts title or content.
+     *
+     * @param value String to search blog posts with.
+     * @return All blog posts containing given parameter.
+     */
     @GetMapping("blogs/search/{value}")
     public Iterable<Article> searchPost(@PathVariable String value) {
         return blogRepository.findArticlesByTitleContainingIgnoreCaseOrContentContainingIgnoreCaseOrderByDateAsc(value,value);
     }
 
+    /**
+     * Deletes comment from repository.
+     *
+     * @param commentId Comments Id to remove.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Status NO_CONTENT if successful, throws UserNotAdminException if user was not an admin or
+     * CannotFindTargetException if target was not found.
+     */
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @DeleteMapping("blogs/comments/{commentId:\\d+}")
     public ResponseEntity<Void> deleteComment(@PathVariable int commentId, Authentication auth) {
@@ -120,6 +186,14 @@ public class BlogController {
         return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
     }
 
+    /**
+     * Adds comment to comment database.
+     *
+     * @param comment Content of comment.
+     * @param articleId Id of article to add comment to.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Status CREATED if successful. Throws CannotFindTargetException if author or blog post cannot be found.
+     */
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @PostMapping("blogs/comments")
     public ResponseEntity<Void> addComment(@RequestParam String comment, @RequestParam int articleId, Authentication auth) {
@@ -133,6 +207,13 @@ public class BlogController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * Return all blog posts comments.
+     *
+     * @param articleId blog posts comments to get.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Comments of blog post with id provided in paramater.
+     */
     @GetMapping("blogs/{articleId:\\d+}/comments")
     public Iterable<CommentLikeResponse> getBlogComments(@PathVariable int articleId, Authentication auth) {
         Optional<User> user = auth != null ? userRepository.findUserByUsername(auth.getName()) : Optional.empty();
@@ -143,6 +224,13 @@ public class BlogController {
         return commentLikeResponses;
     }
 
+    /**
+     * Return comment of a blog containing information if user has liked the comment and amount of likes.
+     *
+     * @param commentId Comment id to get.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Content of comment and information if user has liked the comment and amount of likes.
+     */
     @GetMapping("blogs/comments/{commentId:\\d+}")
     public CommentLikeResponse getComment(@PathVariable int commentId, Authentication auth) {
         Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new CannotFindTargetException(0, "Cannot find comment with id: " + commentId));
@@ -173,17 +261,34 @@ public class BlogController {
         return response;
     }
 
+    /**
+     * Gets all comments in comment repository.
+     *
+     * @return All comments in comment database.
+     */
     @GetMapping("blogs/comments")
     public Iterable<Comment> getComments() {
         return commentRepository.findAll();
     }
 
 
+    /**
+     * Return all likes in like repository.
+     *
+     * @return Likes in like database.
+     */
     @GetMapping("blogs/comments/likes")
     public Iterable<LikeStatus> getLikes() {
         return likeRepository.findAll();
     }
 
+    /**
+     * Returns true if user has liked a blog post. False if not. User is determined by Authentication provided by Spring.
+     *
+     * @param commentId Id of comment to check if user has liked the comment.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return True if has liked. False if not.
+     */
     @GetMapping("blogs/comments/likes/{commentId:\\d+}")
     public boolean hasLiked(@PathVariable int commentId,  Authentication auth) {
         User user = userRepository.findUserByUsername(auth.getName())
@@ -194,6 +299,13 @@ public class BlogController {
         return status.isPresent();
     }
 
+    /**
+     * Adds like to a comment.
+     *
+     * @param commentId Id of comment to like.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return Status CREATED if successful. Throws CannotFindTargetException if authentication user or comment cannot be found.
+     */
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @PostMapping("blogs/comments/likes/{commentId:\\d+}")
     public ResponseEntity<Void> postLike(@PathVariable int commentId, Authentication auth) {
@@ -207,6 +319,13 @@ public class BlogController {
         return new ResponseEntity<>(HttpStatus.CREATED);
     }
 
+    /**
+     * Removes like from a comment.
+     *
+     * @param commentId Id of comment to remove like.
+     * @param auth Authentication provided by Spring. Used to determine user.
+     * @return
+     */
     @Secured({"ROLE_USER", "ROLE_ADMIN"})
     @DeleteMapping("blogs/comments/likes/{commentId:\\d+}")
     public  ResponseEntity<Void> deleteLike(@PathVariable int commentId, Authentication auth) {
