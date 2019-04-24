0443c8546950f5d3e2e9d2f7d26790738b734d7b
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-24 16:34:30 +0300

    ArticleView: Change like button to blue if comment has been liked
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- frontend/src/components/ArticleView.js
+++ frontend/src/components/ArticleView.js
@@ -193,7 +193,7 @@ class ArticleView extends Component {
   renderCommentLike(data) {
     if(this.props.role == "ADMIN" || this.props.role == "USER") {
       if (data.hasLiked) {
-        return <Button icon="pi pi-chevron-up" className="p-button-success" onClick={() => this.dislike(data.link)}/>
+        return <Button icon="pi pi-chevron-up" className="p-button-primary" onClick={() => this.dislike(data.link)}/>
       } else {
         return <Button icon="pi pi-chevron-up" className="p-button-secondary" onClick={() => this.like(data.link)}/>
       }
