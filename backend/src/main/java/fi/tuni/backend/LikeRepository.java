9a121d6a835483ce7ef6c79db72ec97c9ca5f085
Tuukkaj <juusela.tuukka@gmail.com>
2019-04-25 01:10:11 +0300

    LikeRepository: Add Javadoc
    Signed-off-by: Tuukkaj <juusela.tuukka@gmail.com>

--- backend/src/main/java/fi/tuni/backend/LikeRepository.java
+++ backend/src/main/java/fi/tuni/backend/LikeRepository.java
@@ -5,7 +5,30 @@ import org.springframework.data.repository.CrudRepository;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Interface for like management.
+ *
+ * @author Joonas Lauhala {@literal <joonas.lauhala@tuni.fi>}
+ *         Tuukka Juusela {@literal <tuukka.juusela@tuni.fi}
+ * @version 20192802
+ * @since   1.8
+ */
 public interface LikeRepository extends CrudRepository<LikeStatus, Integer> {
+
+    /***
+     * Finds like with given liker id and comment id.
+     *
+     * @param likerId Id of liker.
+     * @param CommentId Id of comment.
+     * @return Optional with like if found. If not found returns Optional with null.
+     */
     Optional<LikeStatus> findByLikerIdAndCommentId(int likerId, int CommentId);
+
+    /**
+     * Finds list of likes by comment id.
+     *
+     * @param id Id of comment.
+     * @return List of comments found with id of comment.
+     */
     List<LikeStatus> findLikeStatusesByCommentId(int id);
 }
